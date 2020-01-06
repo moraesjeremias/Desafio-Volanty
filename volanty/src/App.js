82c35de654126847917916b5e44a74742e1361e9
moraesjeremias <moraesjeremias@users.noreply.github.com>
2020-01-06 15:05:50 -0300

    Description Component Add
    Description Component Add + Fonts Add + SVGs Add + Minor modifications on react app template

--- volanty/src/App.js
+++ volanty/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import VolantyHeader from './components/header';
+import Description from './components/description/index';
 import './styles.css';
 
 const App = () => (
     <div className="App">
       <VolantyHeader />
+      <Description />
     </div>
 );
 
